31d6367e558f146d36fd09b54a48ef953e4c9c17
Ju5t1nL3 <justinle2006@gmail.com>
2026-02-21 15:38:13 -0600

    Add main scene with manager

---
+++ core/game_manager.gd
@@ -0,0 +1,43 @@
+extends Node
+
+@onready var container = $CurrentSceneContainer
+
+# 1. Define your exact progression order here using file paths
+var progression_sequence: Array[String] = [
+	"res://menu/MainMenu.tscn",
+	"res://story/OpeningStory.tscn",
+	"res://minigames/minigame_1/Minigame1.tscn",
+	"res://minigames/minigame_2/Minigame2.tscn",
+	"res://minigames/minigame_3/Minigame3.tscn",
+	"res://story/EndingStory.tscn"
+]
+
+var current_index: int = 0
+
+func _ready():
+	load_scene(current_index)
+
+func load_scene(index: int):
+	# Clear out whatever scene is currently in the container
+	for child in container.get_children():
+		child.queue_free()
+		
+	# Load the new scene from the array
+	var next_scene_resource = load(progression_sequence[index])
+	var next_scene_instance = next_scene_resource.instantiate()
+	
+	# Connect to that universal signal we established earlier!
+	next_scene_instance.sequence_finished.connect(_on_sequence_finished)
+	
+	# Add it to the screen
+	container.add_child(next_scene_instance)
+
+func _on_sequence_finished():
+	current_index += 1
+	if current_index < progression_sequence.size():
+		load_scene(current_index)
+	else:
+		print("Game Over! You beat the progression.")
+		# Optional: Reset back to menu
+		current_index = 0
+		load_scene(current_index)
