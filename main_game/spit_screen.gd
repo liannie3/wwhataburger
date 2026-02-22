46b19a9c68e48255556d3c82445e509a0c67c0f4
Ju5t1nL3 <justinle2006@gmail.com>
2026-02-22 00:04:53 -0600

    Add middle minimap

--- main_game/spit_screen.gd
+++ main_game/spit_screen.gd
@@ -2,10 +2,12 @@ extends Control
 
 @onready var viewport1 = $HBoxContainer/SubViewportContainer/SubViewport
 @onready var viewport2 = $HBoxContainer/SubViewportContainer2/SubViewport2
+@onready var minimap_viewport = $CanvasLayer/CenterContainer/SubViewportContainer/SubViewport
 
 func _ready():
 	# 1. Force the right screen to exist in the exact same physical world as the left screen
 	viewport2.world_2d = viewport1.world_2d
+	minimap_viewport.world_2d = viewport1.world_2d
 	
 	# 2. Grab Player 2's camera from the left screen's game instance
 	var p2_cam = viewport1.get_node("MainGame/Player2/Camera2D")
