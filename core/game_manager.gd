b507f39e9778dcfeadc9275e04517480d95fb0ba
Ju5t1nL3 <justinle2006@gmail.com>
2026-02-22 11:33:20 -0600

    Handle different game states

--- core/game_manager.gd
+++ core/game_manager.gd
@@ -7,12 +7,12 @@ var progression_sequence: Array[String] = [
 	"res://menu/MainMenu.tscn",
 	"res://story/OpeningStory.tscn",
 	"res://main_game/SplitScreen.tscn",
-	"res://story/EndingStory.tscn"
 ]
 
 var current_index: int = 0
 
 func _ready():
+	GlobalStats.game_over.connect(_on_game_over)
 	load_scene(current_index)
 
 func load_scene(index: int):
@@ -24,18 +24,30 @@ func load_scene(index: int):
 	var next_scene_resource = load(progression_sequence[index])
 	var next_scene_instance = next_scene_resource.instantiate()
 	
+	if next_scene_instance.has_signal("sequence_finished"):
 	# Connect to that universal signal we established earlier!
-	next_scene_instance.sequence_finished.connect(_on_sequence_finished)
+		next_scene_instance.sequence_finished.connect(_on_sequence_finished)
 	
 	# Add it to the screen
 	container.add_child(next_scene_instance)
+
+func load_scene_from_path(scene_path: String):
+	for child in container.get_children():
+		child.queue_free()
+		
+	var scene_resource = load(scene_path)
+	var scene_instance = scene_resource.instantiate()
+	container.add_child(scene_instance)
 
 func _on_sequence_finished():
 	current_index += 1
 	if current_index < progression_sequence.size():
 		load_scene(current_index)
-	else:
-		print("Game Over! You beat the progression.")
-		# Optional: Reset back to menu
-		current_index = 0
-		load_scene(current_index)
+
+func _on_game_over(ending_type: GlobalStats.Ending):
+	print("Game Over Triggered! Ending Type: ", ending_type)
+	
+	if ending_type == GlobalStats.Ending.GOOD:
+		load_scene_from_path("res://story/GoodEnding.tscn") # Update this path!
+	elif ending_type == GlobalStats.Ending.BAD:
+		load_scene_from_path("res://story/BadEnding.tscn") # Update this path!
--- core/global_stats.gd
+++ core/global_stats.gd
@@ -1,18 +1,35 @@
 extends Node
 
+enum Ending {
+	GOOD,
+	BAD,
+}
+
 signal stat_changed(player_id: int, stat_name: String, new_value: int)
 signal evidence_changed(player_id: int, new_amount: int)
+signal game_over(ending_type: Ending)
 
 var p1_evidence: int = 0
 var p2_evidence: int = 0
+var is_game_over: bool = false
 
 func add_evidence(player_id: int, amount: int) -> void:
+	if is_game_over: return
+
 	if player_id == 1:
 		p1_evidence += amount
 		evidence_changed.emit(1, p1_evidence)
+		if p1_evidence >= 10:
+			is_game_over = true
+			# CHANGE 3: Emit the enum instead of the string!
+			game_over.emit(Ending.BAD)
 	elif player_id == 2:
 		p2_evidence += amount
 		evidence_changed.emit(2, p2_evidence)
+		if p2_evidence >= 10:
+			is_game_over = true
+			# CHANGE 4: Emit the enum instead of the string!
+			game_over.emit(Ending.BAD)
 
 var p1_first_talks: int = 0:
 	set(value):
@@ -23,3 +40,11 @@ var p2_first_talks: int = 0:
 	set(value):
 		p2_first_talks = value
 		stat_changed.emit(2, "first_talks", value)
+
+var buildings_visited: int = 0:
+	set(value):
+		buildings_visited = value
+		if buildings_visited >= 3 and not is_game_over:
+			is_game_over = true
+			# CHANGE 2: Emit the enum instead of the string!
+			game_over.emit(Ending.GOOD)
