a1a5109496536b722494ee4168721cf8d4871cc9
Eric Li <eric.li.8736@gmail.com>
2026-02-21 20:53:17 -0600

    Change direction keys based on the player

---
+++ main_game/player.gd
@@ -0,0 +1,19 @@
+extends CharacterBody2D
+
+@export var player_id: int = 1 
+var speed: float = 300.0
+
+func _physics_process(delta: float) -> void:
+	# 1. Build the action strings dynamically based on the player_id
+	# If player_id is 1, this becomes "p1_up". If it's 2, it becomes "p2_up".
+	var up = "p" + str(player_id) + "_up"
+	var down = "p" + str(player_id) + "_down"
+	var left = "p" + str(player_id) + "_left"
+	var right = "p" + str(player_id) + "_right"
+
+	# 2. Get the movement direction using our dynamically created strings
+	var direction = Input.get_vector(left, right, up, down)
+	
+	# 3. Apply the movement
+	velocity = direction * speed
+	move_and_slide()
