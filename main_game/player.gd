e4b1081d5758f950155f8bf93dc9226544ffba36
Ju5t1nL3 <justinle2006@gmail.com>
2026-02-21 21:20:04 -0600

    Use reusable prefix for animations

--- main_game/player.gd
+++ main_game/player.gd
@@ -8,10 +8,11 @@ extends CharacterBody2D
 func _physics_process(delta: float) -> void:
 	# 1. Build the action strings dynamically based on the player_id
 	# If player_id is 1, this becomes "p1_up". If it's 2, it becomes "p2_up".
-	var up = "p" + str(player_id) + "_up"
-	var down = "p" + str(player_id) + "_down"
-	var left = "p" + str(player_id) + "_left"
-	var right = "p" + str(player_id) + "_right"
+	var prefix = "p" + str(player_id)
+	var up = prefix + "_up"
+	var down = prefix + "_down"
+	var left = prefix + "_left"
+	var right = prefix + "_right"
 
 	# 2. Get the movement direction using our dynamically created strings
 	var direction = Input.get_vector(left, right, up, down)
@@ -22,11 +23,7 @@ func _physics_process(delta: float) -> void:
 	
 	if direction.length() > 0:
 		# If moving, play the move animation
-		anim.play("move")
-		
-		# Flip the sprite left or right depending on the X direction
-		if direction.x != 0:
-			anim.flip_h = direction.x < 0 
+		anim.play(prefix + "move")
 	else:
 		# If standing still, play idle
-		anim.play("idle")
+		anim.play(prefix + "idle")
