6ff828802dbdd9a92eeff135079e95b82b7f86ee
Eric Li <eric.li.8736@gmail.com>
2026-02-22 03:26:53 -0600

    Add helpers to global_stats for evidence

--- core/global_stats.gd
+++ core/global_stats.gd
@@ -1,7 +1,16 @@
 extends Node
 
-# These variables will exist forever and can be accessed from ANY script!
+signal evidence_changed(player_id: int, new_amount: int)
+
 var p1_first_talks: int = 0
 var p2_first_talks: int = 0
+var p1_evidence: int = 0
+var p2_evidence: int = 0
 
-# You can add health, money, or speed stats here later!
+func add_evidence(player_id: int, amount: int) -> void:
+    if player_id == 1:
+        p1_evidence += amount
+        evidence_changed.emit(1, p1_evidence)
+    elif player_id == 2:
+        p2_evidence += amount
+        evidence_changed.emit(2, p2_evidence)
