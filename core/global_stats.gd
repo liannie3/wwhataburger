5007c17316aa7c3bc060d6de7ee7faada4006a5b
Ju5t1nL3 <justinle2006@gmail.com>
2026-02-22 02:16:01 -0600

    Add global stats file

---
+++ core/global_stats.gd
@@ -0,0 +1,7 @@
+extends Node
+
+# These variables will exist forever and can be accessed from ANY script!
+var p1_first_talks: int = 0
+var p2_first_talks: int = 0
+
+# You can add health, money, or speed stats here later!
