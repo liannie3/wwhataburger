81f73077bfa77235df6a73bd93287d3cf84d7d8d
William Lam <williamlam5656@gmail.com>
2026-02-23 09:14:27 -0600

    new comment for annie

--- main_game/main_game.gd
+++ main_game/main_game.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 signal sequence_finished
 #hello comments
-
+#hahahahahha annie is a loser
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
